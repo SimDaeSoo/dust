c1fc8d32b623336c58c493bc1d2c7cfbab1fb24e
daesoo94 <tlaeotn123@gmail.com>
2021-08-22 18:16:47 +0900

    Animated Sprite Sample Test

--- client/src/renderer.ts
+++ client/src/renderer.ts
@@ -2,10 +2,19 @@ import * as PIXI from 'pixi.js';
 import './index.css';
 
 const resolution = window.devicePixelRatio || 1;
+PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
+PIXI.settings.MIPMAP_TEXTURES = PIXI.MIPMAP_MODES.OFF;
+// PIXI.settings.STRICT_TEXTURE_CACHE = true;
+
 const app = new PIXI.Application({
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: 0x000000,
+  sharedLoader: true,
+  powerPreference: 'high-performance',
+  antialias: false,
+  forceCanvas: false,
+  preserveDrawingBuffer: false,
   resolution,
 });
 
@@ -15,32 +24,25 @@ app.view.style.height = '100%';
 document.body.appendChild(app.view);
 
 const container = new PIXI.Container();
-
-app.stage.addChild(container);
-
-// Create a new texture
-const texture = PIXI.Texture.from('../assets/characters/1.png');
-
-// Create a 5x5 grid of bunnies
-for (let i = 0; i < 25; i++) {
-  const bunny = new PIXI.Sprite(texture);
-  bunny.anchor.set(0.5);
-  bunny.x = (i % 5) * 40;
-  bunny.y = Math.floor(i / 5) * 40;
-  container.addChild(bunny);
+for (let i = 0; i < 118; i++) {
+  const src = `../assets/characters/body/1/${i.toString().padStart(3, '0')}.png`;
+  PIXI.Loader.shared.add(src, src);
 }
 
-// Move container to the center
-container.x = app.screen.width / 2;
-container.y = app.screen.height / 2;
+PIXI.Loader.shared.load(() => {
+  const textures = [];
+  for (let i = 0; i < 118; i++) {
+    const src = `../assets/characters/body/1/${i.toString().padStart(3, '0')}.png`;
+    textures.push(PIXI.Texture.from(src));
+  }
 
-// Center bunny sprite in local container coordinates
-container.pivot.x = container.width / 2;
-container.pivot.y = container.height / 2;
+  const animatedSprite = new PIXI.AnimatedSprite(textures);
+  animatedSprite.width = 128;
+  animatedSprite.height = 128;
+  animatedSprite.animationSpeed = 0.2;
+  animatedSprite.play();
 
-// Listen for animate update
-app.ticker.add((delta) => {
-  // rotate the container!
-  // use delta to create frame-independent transform
-  container.rotation -= 0.01 * delta;
+  container.addChild(animatedSprite);
 });
+
+app.stage.addChild(container);
