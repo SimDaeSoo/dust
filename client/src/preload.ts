fb9c3a76ce9d44c17833e17ab64099b6253a06ec
daesoo94 <tlaeotn123@gmail.com>
2021-09-10 20:08:17 +0900

    preload - IPC renderer와 통신속도 확인.

--- client/src/preload.ts
+++ client/src/preload.ts
@@ -1,1 +1,28 @@
-console.log('hi im preload ts');
+import { Grid, Map, MapData, Tile } from "@dust/core";
+import { contextBridge } from "electron";
+
+let mapData: MapData;
+contextBridge.exposeInMainWorld(
+  "electron", {
+  getMap: (): MapData => mapData,
+  createMap: (): void => {
+    mapData = Map.generate({
+      seed: `${Math.random()}`,
+      width: 100,
+      height: 100,
+      tileSize: 32,
+      step: 4,
+      clearTop: 0,
+      tileTypes: [4],
+      density: {
+        block: 0.3,
+        liquid: 0
+      },
+      birthLimit: 3,
+      deathLimit: 2,
+      liquidLimit: 0
+    });
+    console.log(mapData);
+  }
+}
+);
